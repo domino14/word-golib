60a09171ac1c7388cf9fbc2b1643956684d67ff7
Hultarn <phultus@hotmail.com>
2026-07-02 15:29:36 +0200

    added swedish

--- tilemapping/letter_distribution.go
+++ tilemapping/letter_distribution.go
@@ -158,6 +158,7 @@ func ProbableLetterDistributionName(lexname string) (string, error) {
 		"file":    "spanish",
 		"dsw":     "dutch",
 		"slv":     "slovene",
+		"swedish": "swedish",
 	}
 	for prefix, name := range prefixMap {
 		if strings.HasPrefix(lexname, prefix) {
